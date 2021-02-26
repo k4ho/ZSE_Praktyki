7f598bbfa51ee0fd496ae518ca66a5984f9a6ce8
k4ho <k.klos780@gmail.com>
2021-02-26 11:21:29 +0100

    Zadanka 1-4 chyba ok

---
+++ src/Zadanka.java
@@ -0,0 +1,33 @@
+public class Zadanka {
+    //zadanie 1
+
+    int myIntA = 17;
+    double myDoubleB = 4.2;
+    char myCharC = '&';
+    String myStringD = "Hamburger";
+
+
+
+
+    //zadanie 2
+
+    int A = 10;
+    int B = 5;
+
+    int sum;
+    sum = A*A + B*B;
+
+
+    //zadanie 3
+
+    double sqrt = Math.sqrt(3);
+    int myIntBok = 7;
+
+    double Wynik = myIntBok*myIntBok * sqrt /4;
+
+
+    //zadanie 4
+
+    String myStringA = "Kacper";
+    String myStringB = "Kłos";
+}
